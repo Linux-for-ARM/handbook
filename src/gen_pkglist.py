3d792131e1e176f983e8bc46b30756032d3ccb06
mskrasnov <linuxoid85@gmail.com>
2024-03-03 13:42:47 +0300

    Изменение формата списка пакетов

--- src/gen_pkglist.py
+++ src/gen_pkglist.py
@@ -7,9 +7,9 @@ PKGS_MD = "../md/pkgs.md" # Markdown-страница со списком пак
 PKGS_TOML = "../packages.toml" # Файл со списком пакетов
 
 def get_md_content(pkg: str, packages: dict) -> str:
-    return f"- **{pkg} ({packages[pkg]['version']})** - {packages[pkg]['description']}<br>\
-- Домашняя страница: [{packages[pkg]['home_page']}]({packages[pkg]['home_page']})<br>\
-- Скачать: [{packages[pkg]['download']}]({packages[pkg]['download']})<br>\
+    return f"## {pkg}-{packages[pkg]['version']}\n{packages[pkg]['description']}\n\
+- Домашняя страница: <{packages[pkg]['home_page']}>\n\
+- Скачать: <{packages[pkg]['download']}>\n\
 - MD5 сумма: `{packages[pkg]['md5']}`\n"
 
 packages: dict = toml.load(PKGS_TOML)["package"]
