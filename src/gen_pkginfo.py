afe58afa8682aab5aa29ec903e4ed14ed0a79299
mskrasnov <linuxoid85@gmail.com>
2024-03-28 16:03:35 +0300

    Fix #13

--- src/gen_pkginfo.py
+++ src/gen_pkginfo.py
@@ -17,7 +17,7 @@ DIRS = {
 
 packages = toml.load(PKGS_ALL)['package']
 pkg_index = toml.load(PKG_INDEX)['package']
-#print(pkg_index)
+# print(pkg_index)
 
 for d in DIRS.values():
 	if not d.exists():
@@ -29,13 +29,27 @@ for d in DIRS.values():
 			# но мне лень указывать конкретное исключение
 			print(f"[gen_pkginfo] error!")
 
-def get_md_str(pkg_name: str, pkg_lst: dict, pkg_idx: dict) -> str:
-	return f"> {pkg_idx[pkg_name]['description']}\n\
-> - **Версия:** {pkg_lst[pkg_name]['version']}\n\
-> - **Домашняя страница:** [{pkg_lst[pkg_name]['home_page']}]({pkg_lst[pkg_name]['home_page']})\n\
-> - **Ссылка для загрузки:** [{pkg_lst[pkg_name]['download']}]({pkg_lst[pkg_name]['download']})\n\
-> - **MD5-сумма:** `{pkg_lst[pkg_name]['md5']}`\n\
-> - **ОВС:** {pkg_idx[pkg_name]['sbu']}\n"
+# def get_md_str(pkg_name: str, pkg_lst: dict, pkg_idx: dict) -> str:
+# 	description = ""
+# 	if pkg_idx.get("description"):
+# 		description = pkg_idx["description"]
+# 	else:
+# 		description = pkg_lst["description"]
+
+# 	return f"> {description}\n\
+# > - **Версия:** {pkg_lst[pkg]['version']}\n\
+# > - **ОВС:** {pkg_idx[pkg_name]['sbu']}\n"
+
+def get_md_str(pkg: dict, pkg_lst: dict):
+	descr = ""
+	if pkg.get("description"):
+		descr = pkg["description"]
+	else:
+		descr = pkg_lst[pkg["package"]]["description"]
+
+	return f"> {descr}\n\
+> - **Версия:** {pkg_lst[pkg['package']]['version']}\n\
+> - **Время сборки:** {pkg['sbu']} ОВС\n"
 
 def write_to_pkg_md(data: str, pkg_id: str, dir_pth: str):
 	pth = DIRS[dir_pth].joinpath(f"{pkg_id}.md")
@@ -43,8 +57,8 @@ def write_to_pkg_md(data: str, pkg_id: str, dir_pth: str):
 	with open(pth, 'w') as f:
 		f.write(data)
 
-for pkg in pkg_index.keys():
-	print(f"write information about {pkg_index[pkg]['dir_pth']}/{pkg_index[pkg]['id']}...")
+for pkg in pkg_index:
+	print(f"write information about {pkg['dir_pth']}/{pkg['id']}...")
 
-	data = get_md_str(pkg, packages, pkg_index)
-	write_to_pkg_md(data, pkg_index[pkg]['id'], pkg_index[pkg]['dir_pth'])
+	data = get_md_str(pkg, packages)
+	write_to_pkg_md(data, pkg['id'], pkg['dir_pth'])
