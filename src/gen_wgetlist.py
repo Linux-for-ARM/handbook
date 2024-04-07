7fa39eeab71492d989e10b37c5bc3301336c627d
mskrasnov <linuxoid85@gmail.com>
2024-04-07 16:42:40 +0300

    Очистка wget-list перед генерацией

--- src/gen_wgetlist.py
+++ src/gen_wgetlist.py
@@ -8,10 +8,14 @@ WGET_LST_PTH = Path("../wget-list")
 PKG_IDX_PTH = Path("../packages.toml")
 
 pkg_idx = toml.load(PKG_IDX_PTH)['package']
+pkg_urls = ""
 
 for pkg in pkg_idx.keys():
     if pkg_idx[pkg].get('display') is not None:
         continue
     url = pkg_idx[pkg]['download']
-    with open(WGET_LST_PTH, 'a') as f:
-        f.write(f"{url}\n")
+    pkg_urls = f"{pkg_urls}{url}\n"
+
+pkg_urls.strip()
+with open(WGET_LST_PTH, 'w') as f:
+    f.write(f"{pkg_urls}\n")
